f6ce3903f4720137ebdc0fa3ee6f56ff41ef5158
Fanariotis Dimitris <83169908+BionicReaper@users.noreply.github.com>
2026-05-11 19:42:08 +0300

    Created algorithm specific translation files

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -1,13 +1,26 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translations from '../locales/translations.json';
+import kernighanlinTranslations from '../locales/kernighan-lin.json';
+import fiducciamattheysesTranslations from '../locales/fiduccia-mattheyses.json';
+import metisTranslations from '../locales/METIS.json';
 
 const resources = {
   en: {
-    translation: translations.en
+    translation: {
+      ...translations.en,
+      ...kernighanlinTranslations.en,
+      ...fiducciamattheysesTranslations.en,
+      ...metisTranslations.en
+    }
   },
   el: {
-    translation: translations.el
+    translation: {
+      ...translations.el,
+      ...kernighanlinTranslations.el,
+      ...fiducciamattheysesTranslations.el,
+      ...metisTranslations.el
+    }
   }
 };
 
